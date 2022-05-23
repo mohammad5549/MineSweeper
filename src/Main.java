3c8725c5fd01e3ead6213fb0bf985f36f6f7bab1
msteenhuis <moizs@nycstudents.net>
2022-05-23 12:32:19 -0400

    bozo activity

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.awt.*;
 import java.util.Arrays;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board(2);
+       /* Board b = new Board(2);
         b.generateBombs();
         b.generateNumber(0, 0);
         String[][] board = b.getBoard();
@@ -10,6 +13,107 @@ public class Main {
         {
             System.out.println(Arrays.toString(x));
         }
-        //UserInterface.setupGui();
+        //UserInterface.setupGui();*/
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(625, 500);
+        frame.setLayout(new GridLayout(8, 10, 2, 2));
+
+        for (int i = 1; i <= 80; i++){
+            JButton button = new JButton("");
+            if (i % 2 == 0) {
+                button.setBackground(Color.GRAY);
+            }
+            else {
+                button.setBackground(Color.DARK_GRAY);
+            }
+            frame.add(button);
+        }
+
+        /*frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));
+        frame.add(new JButton(""));*/
+
+        frame.setVisible(true);
+
     }
 }
---
+++ src/MyFrame.java
@@ -0,0 +1,33 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements MouseListener {
+
+    JLabel label;
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
