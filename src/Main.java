da6469e26a331df7ab40b594c38b59d73dd5087c
mohammad5549 <miqbal5549@bths.edu>
2022-05-23 23:51:01 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+       MyFrame frame = new MyFrame();
+
+        //frame.setLayout(new GridLayout(8, 10, 2, 2));
+
        /* Board b = new Board(2);
         b.generateBombs();
         b.generateNumber(0, 0);
@@ -16,104 +20,20 @@ public class Main {
         //UserInterface.setupGui();*/
 
 
-        JFrame frame = new JFrame();
+        /*JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(625, 500);
         frame.setLayout(new GridLayout(8, 10, 2, 2));
 
+
         for (int i = 1; i <= 80; i++){
             JButton button = new JButton("");
-            if (i % 2 == 0) {
-                button.setBackground(Color.GRAY);
-            }
-            else {
-                button.setBackground(Color.DARK_GRAY);
-            }
+            button.setBackground(Color.GRAY);
             frame.add(button);
-        }
+        }*/
 
-        /*frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));
-        frame.add(new JButton(""));*/
 
-        frame.setVisible(true);
+
 
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,33 +1,53 @@
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MyFrame extends JFrame implements MouseListener {
+public class MyFrame extends JFrame implements ActionListener {
 
-    JLabel label;
+    JButton level1;
+    JButton level2;
+    JButton level3;
 
+    public MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(625, 500);
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
+        level1 = new JButton("Level 1");
+        level1.setBounds(250, 100, 100, 50);
+        this.add(level1);
+        level1.addActionListener(this);
 
+        level2 = new JButton("Level 2");
+        level2.setBounds(250, 150, 100, 50);
+        this.add(level2);
+        level2.addActionListener(this);
+
+        level3 = new JButton("Level 3");
+        level3.setBounds(250, 200, 100, 50);
+        this.add(level3);
+        level3.addActionListener(this);
+
+        this.setVisible(true);
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == level1) {
+            this.remove(level1);
+            this.remove(level2);
+            this.remove(level3);
+            level1.setVisible(false);
+            level2.setVisible(false);
+            level3.setVisible(false);
+            this.setLayout(new GridLayout(8, 10, 2, 2));
+            for (int i = 1; i <= 80; i++){
+                JButton button = new JButton("");
+                button.setBackground(Color.GRAY);
+                this.add(button);
+            }
+            this.setVisible(true);
+        }
     }
 }
